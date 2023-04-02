7567cc3a6e10cd98250acfc996b4806c22f3b2d3
otabekaripdjanov <597066@park.edu>
2023-04-02 14:53:03 -0400

    step 3

--- src/CaveExplorer.java
+++ src/CaveExplorer.java
@@ -41,4 +41,56 @@ public class CaveExplorer {
 
         return sb.toString();
     }
+
+    // Step 3: solve method
+    public boolean solve() {
+        int r = this.startRow;
+        int c = this.startCol;
+
+        // iterate through the cells in the path
+        while (r < this.rows && c < this.cols) {
+
+            // check if we are  at the target
+            if (this.cave[r][c] == 'M') {
+                return true;
+            }
+
+            // Mark the location to avoid looping
+            this.cave[r][c] = 'S';
+
+            // search for path now in all four directions [N,S,W,E]
+            if(this.validCell(r-1,c)) {
+                this.path += "n";
+                r--;
+            } else if (this.validCell(r+1, c)) {
+                this.path += "s";
+                r++;
+            } else if (this.validCell(r, c-1)) {
+                this.path += "w";
+                c--;
+            } else if (this.validCell(r, c+1)) {
+                this.path += "e";
+                c++;
+            } else {
+                return false;
+            }
+        }
+
+        // No valid path
+        return false;
+    }
+
+    public boolean validCell(int r, int c) {
+        // 2D array outOfBounds check
+        if (r >= this.cave.length || r < 0 || c < 0 | c >= this.cave[0].length) {
+            return false;
+        }
+
+        // check if the move in a path is possible
+        if (this.cave[r][c] == 'S' || this.cave[r][c] == 'R') {
+            return false;
+        }
+
+        return true;
+    }
 }
